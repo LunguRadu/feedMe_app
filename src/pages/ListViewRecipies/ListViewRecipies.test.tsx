e3c6891fc834402424d6ab05b416fcb6ef70b1b6
Henry <hbasu@macalester.edu>
2020-04-07 09:44:31 -0700

    deleted ListViewRecipe and some code that was commented out in SearchPage

--- src/pages/ListViewRecipies/ListViewRecipies.test.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import ListViewRecipies from './ListViewRecipies';
-
-// test('renders learn react link', () => {
-//   const { getByText } = render(<ListViewRecipies />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
