fb222835844a0b5a1b465b0919a5b838f56a13de
LunguRadu <lungu.radu123@gmail.com>
2020-04-23 12:19:37 -0500

    made the ion-items in the list as a full button. put the button is a grid

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from 'react';
-import './SearchPage.css';
-import {AppContext } from '../../data/AppContext';
-import {possibleIngredients} from '../../data/ingredients/possibleIngredients';
-import {listOfIngredients} from '../../data/ingredients/userIngredients';
+import React, { useState, useContext } from "react";
+import "./SearchPage.css";
+import { AppContext } from "../../data/AppContext";
+import { possibleIngredients } from "../../data/ingredients/possibleIngredients";
+import { listOfIngredients } from "../../data/ingredients/userIngredients";
 import {
   IonButton,
   IonButtons,
@@ -15,13 +15,19 @@ import {
   IonList,
   IonItem,
   IonIcon,
-  IonTabs
-} from '@ionic/react';
-import { RouteComponentProps } from 'react-router';
-import { attachProps } from '@ionic/react/dist/types/components/utils';
-import { setSearchText, loadRecipeData } from '../../data/recipes/recipes.actions';
-import { connect } from '../../data/connect';
-import { playOutline } from 'ionicons/icons';
+  IonTabs,
+  IonRow,
+  IonCol,
+  IonGrid,
+} from "@ionic/react";
+import { RouteComponentProps } from "react-router";
+import { attachProps } from "@ionic/react/dist/types/components/utils";
+import {
+  setSearchText,
+  loadRecipeData,
+} from "../../data/recipes/recipes.actions";
+import { connect } from "../../data/connect";
+import { playOutline } from "ionicons/icons";
 
 interface StateProps {
   id: string;
@@ -35,32 +41,37 @@ interface DispatchProps {
 
 type SearchPageProps = StateProps & DispatchProps;
 
-const SearchPage: React.FC<SearchPageProps> = ({history, loadRecipeData }) => {
-
-  const [IngredientText, setSearchText] = useState('');
+const SearchPage: React.FC<SearchPageProps> = ({ history, loadRecipeData }) => {
+  const [IngredientText, setSearchText] = useState("");
   const { state, dispatch } = useContext(AppContext);
 
-  var currentText:string="";
-  var url:string=addTwoStrings("/home?inputs=",listOfIngredients.toString());
-  var paragraph:HTMLIonListElement = document.getElementById('ingredientsList') as HTMLIonListElement;
-  var searchBar:HTMLIonSearchbarElement = document.getElementById('searchBar') as HTMLIonSearchbarElement;
+  var currentText: string = "";
+  var url: string = addTwoStrings(
+    "/home?inputs=",
+    listOfIngredients.toString()
+  );
+  var paragraph: HTMLIonListElement = document.getElementById(
+    "ingredientsList"
+  ) as HTMLIonListElement;
+  var searchBar: HTMLIonSearchbarElement = document.getElementById(
+    "searchBar"
+  ) as HTMLIonSearchbarElement;
 
   //TODO: Move this to seperate tsx file
-  function addButton(){
-    if(currentText===""){
-      return 
-    }
-
-    else if (listOfIngredients.includes(currentText.toLowerCase().replace(/\s/g, ""))){
-      return 
+  function addButton() {
+    if (currentText === "") {
+      return;
+    } else if (
+      listOfIngredients.includes(currentText.toLowerCase().replace(/\s/g, ""))
+    ) {
+      return;
     }
 
     // else if (possibleIngredients.includes(currentText.toLowerCase().replace(/\s/g, ""))){
-    else{
-    // setSearchText("");
-    listOfIngredients.push(currentText.toLowerCase().replace(/\s/g, ""))
-    paragraph.innerHTML=(
-      listOfIngredients.toString().replace(/,/g, ", ")
+    else {
+      // setSearchText("");
+      listOfIngredients.push(currentText.toLowerCase().replace(/\s/g, ""));
+      paragraph.innerHTML = listOfIngredients.toString().replace(/,/g, ", ");
       // {
       //     listOfIngredients.map((m)=>{
       //       for(var _i = 0; _i < listOfIngredients.length; _i++){
@@ -69,106 +80,120 @@ const SearchPage: React.FC<SearchPageProps> = ({history, loadRecipeData }) => {
       //         )
       //       }
       //     })
-      //   } 
-    )
-    url=addTwoStrings("/home?inputs=",listOfIngredients.join().replace(/,/gi,"+").toString());
-    return
+      //   }
+      url = addTwoStrings(
+        "/home?inputs=",
+        listOfIngredients.join().replace(/,/gi, "+").toString()
+      );
+      return;
     }
 
     // return
   }
 
-  function enterKeyPress(e:KeyboardEvent){
-    if(e.keyCode===13){
+  function enterKeyPress(e: KeyboardEvent) {
+    if (e.keyCode === 13) {
       addButton();
     }
   }
 
-  function enterEvent(){
-    searchBar.addEventListener("keyup", (e)=>{enterKeyPress(e)})
+  function enterEvent() {
+    searchBar.addEventListener("keyup", (e) => {
+      enterKeyPress(e);
+    });
   }
 
-  function clearList(){
-    for(var _i = 0; _i <= listOfIngredients.length; _i++){
-      listOfIngredients.pop()
+  function clearList() {
+    for (var _i = 0; _i <= listOfIngredients.length; _i++) {
+      listOfIngredients.pop();
     }
     url = "";
-    currentText=""
-    paragraph.innerHTML=("***")
+    currentText = "";
+    paragraph.innerHTML = "***";
   }
 
-  function removeOneIngredient(){
+  function removeOneIngredient() {
     url = "";
-    currentText=""
+    currentText = "";
     listOfIngredients.pop();
-    paragraph.innerHTML=(
-      listOfIngredients.toString().replace(/,/g, ", ")
-    )
+    paragraph.innerHTML = listOfIngredients.toString().replace(/,/g, ", ");
   }
 
-  function addTwoStrings(s1:string,s2:string){
+  function addTwoStrings(s1: string, s2: string) {
     return s1.concat(s2);
   }
 
-  function addFromScrollList(s:string){
-    currentText=s
-    addButton()
-    currentText=""
+  function addFromScrollList(s: string) {
+    currentText = s;
+    addButton();
+    currentText = "";
   }
 
   return (
-    
     <IonPage id="home-page">
-       <IonHeader>
-      <h1>#feedMe</h1>
+      <IonHeader>
+        <h1>#feedMe</h1>
       </IonHeader>
       <IonToolbar>
-        <b>Select Ingredients:</b>
-        <IonSearchbar placeholder = "type ingredients..."id = "searchBar" value={IngredientText} onIonChange={e => currentText=(e.detail.value!)} onIonFocus={()=>enterEvent()}>
-        </IonSearchbar>
+        <p>Select Ingredients:</p>
+        <IonSearchbar
+          placeholder="type ingredients..."
+          id="searchBar"
+          value={IngredientText}
+          onIonChange={(e) => (currentText = e.detail.value!)}
+          onIonFocus={() => enterEvent()}
+        ></IonSearchbar>
       </IonToolbar>
 
-
       <IonContent>
-      <div id = 'possibleSearches'>
-        <IonList inset class="bg-transparent" lines="none">
-          {
-            possibleIngredients.map((n)=>{
-              for(var _i = 0; _i < possibleIngredients.length; _i++){
-                return(
-                <IonItem><IonButton shape="round" size="small" fill ="clear" color="success" onClick={()=>addFromScrollList(n)}>{n}</IonButton></IonItem>
-                )
+        <div id="possibleSearches">
+          <IonList inset class="bg-transparent" lines="none">
+            {possibleIngredients.map((n) => {
+              for (var _i = 0; _i < possibleIngredients.length; _i++) {
+                return (
+                  <IonItem button lines="inset" >
+                    <IonButton size="default" fill="clear" color="warning" onClick={() => addFromScrollList(n)}>
+                      {n}
+                    </IonButton>
+                  </IonItem>
+                );
               }
-            })
-          }
-        </IonList>
-      </div>
+            })}
+          </IonList>
+        </div>
       </IonContent>
 
-
       <IonFooter translucent>
-      <div>
-        <p>Your ingredients:</p>
-        <IonList id='ingredientsList'> *** </IonList>
-      </div>
+        <div>
+          <p>Your ingredients:</p>
+          <IonList id="ingredientsList"> *** </IonList>
+        </div>
 
-      <IonToolbar >
+        <IonToolbar>
+         <IonGrid>
+            <IonRow className="ion-align-items-center">
           <IonButtons>
-          <IonButton size="large" onClick = {() => addButton()}> Add(+) </IonButton>
-          <IonButton size="large" onClick = {() => removeOneIngredient()}> Delete(-)  </IonButton>
-          <IonButton size="large" onClick = {()=>{clearList();}}> Clear </IonButton>
-          <IonButton size="large" class="seach-button"     
-                      onClick = { 
-                          e => {
-                          e.preventDefault();
-                          loadRecipeData(listOfIngredients.toString());
-                          history.push('/recipelist');
-                        }
-                      }
-                      >
-                        SEARCH
-                      </IonButton>
+            
+              <IonCol>
+            <IonButton size="large" onClick={() => addButton()}>{" "}Add(+){" "}</IonButton>
+             </IonCol>
+
+             <IonCol>
+            <IonButton size="large" onClick={() => removeOneIngredient()}>{" "}Delete(-){" "}</IonButton>
+             </IonCol>
+
+             <IonCol>
+            <IonButton size="large" onClick={() => { clearList();}}>{" "}Clear{" "}</IonButton>
+             </IonCol>
+
+             <IonCol>
+            <IonButton size="large" onClick={(e) => {e.preventDefault();loadRecipeData(listOfIngredients.toString()); history.push("/recipelist");}}>SEARCH</IonButton>
+            </IonCol>
+
+           
           </IonButtons>
+          </IonRow>
+         </IonGrid>
         </IonToolbar>
       </IonFooter>
     </IonPage>
@@ -178,8 +203,7 @@ const SearchPage: React.FC<SearchPageProps> = ({history, loadRecipeData }) => {
 export default connect<StateProps, DispatchProps>({
   mapDispatchToProps: {
     setSearchText,
-    loadRecipeData
+    loadRecipeData,
   },
-  component: React.memo(SearchPage)
+  component: React.memo(SearchPage),
 });
-
